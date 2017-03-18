b9a62937a203e2562312a7309e2ab6cb171dd7ff
Julien_Domptail <juliendomptail@hotmail.com>
2017-03-18 23:19:42 +0100

    ajout image nuage et etoile

--- src/game3/world/Cloud.java
+++ src/game3/world/Cloud.java
@@ -2,10 +2,19 @@ package game3.world;
 
 public class Cloud extends SkyElements{
 
+	private int numberImageCloud;
 	
-	
-	public Cloud(int posX, int posY, int width, int height) {
+	public Cloud(int posX, int posY, int width, int height,int numberImageCloud) {
 		super(posX,posY,width,height);
+		this.numberImageCloud=numberImageCloud;
+	}
+
+	public int getNumberImageCloud() {
+		return numberImageCloud;
+	}
+
+	public void setNumberImageCloud(int numberImageCloud) {
+		this.numberImageCloud = numberImageCloud;
 	}
 	
 }
