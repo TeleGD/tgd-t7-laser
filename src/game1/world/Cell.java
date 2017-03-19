008ab738fcdd9744ca2565b9ce6fcb8a36ab291b
canalarthur <canalarthur@yahoo.fr>
2017-03-19 05:45:54 +0100

    Adding new sprite choices

--- src/game1/world/Cell.java
+++ src/game1/world/Cell.java
@@ -156,14 +156,6 @@ public class Cell{
 	public void render(GameContainer arg0, StateBasedGame arg1, Graphics arg2) throws SlickException 
 	{
 		arg2.drawImage(sprite,j*64,i*64);
-		if (itsATrap){
-			arg2.setColor(Color.red);
-			arg2.fillRect(j*62, i*62, 60, 60);
-		}
-		if (isFinalCell){
-			arg2.setColor(Color.cyan);
-			arg2.fillRect(j*62, i*62, 60, 60);
-		}
 	}
 
 	
@@ -171,6 +163,7 @@ public class Cell{
 		if (this.equals(World1.getPlayer())){
 			if (giveScore){
 				giveScore=false;
+				autoSetSprite();
 				World1.setScore(World1.getScore()+1);
 			}
 			if (isFinalCell){
